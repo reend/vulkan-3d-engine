b7adcb47a79e2f1eb55bc645ffeb827556f70461
Kyrylo Prylipko <kyrylo.pr@oxoix.com>
2026-07-22 23:34:55 +0200

    vertex buffers

--- first_app.cpp
+++ first_app.cpp
@@ -9,6 +9,7 @@ namespace lve
 {
     FirstApp::FirstApp()
     {
+        loadModels();
         createPipelineLayout();
         createPipeline();
         createCommandBuffers();
@@ -17,6 +18,18 @@ namespace lve
     FirstApp::~FirstApp()
     {
         vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr);
+    }
+
+    void FirstApp::loadModels()
+    {
+        std::vector<LveModel::Vertex> vertices 
+        {
+            {{0.0f, -0.5f}},
+            {{0.5f, 0.5f}},
+            {{-0.5f, 0.5f}}
+        };
+
+        lveModel = std::make_unique<LveModel>(lveDevice, vertices);
     }
 
     void FirstApp::run()
@@ -99,7 +112,8 @@ namespace lve
             vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
             
             lvePipeline->bind(commandBuffers[i]);
-            vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+            lveModel->bind(commandBuffers[i]);
+            lveModel->draw(commandBuffers[i]);
 
             vkCmdEndRenderPass(commandBuffers[i]);
             if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS)
